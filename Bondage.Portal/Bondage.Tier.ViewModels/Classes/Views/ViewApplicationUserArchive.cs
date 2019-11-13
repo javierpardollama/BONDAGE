02a0b08c2ea3ee78c751d436bc7b060f314bb13a
Javier Pardo <javierpardollama@outlook.com>
2019-11-13 21:08:01 +0100

    ETIQUETAS DE SERIALIZACION DE XML

--- Bondage.Portal/Bondage.Tier.ViewModels/Classes/Views/ViewApplicationUserArchive.cs
+++ Bondage.Portal/Bondage.Tier.ViewModels/Classes/Views/ViewApplicationUserArchive.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Xml.Serialization;
+
 using Bondage.Tier.ViewModels.Interfaces.Views;
 
 namespace Bondage.Tier.ViewModels.Classes.Views
 {
+    [XmlRoot("application-user-archive")]
     public class ViewApplicationUserArchive : IViewBase, IViewKey
     {
         public ViewApplicationUserArchive()
         {
         }
 
+        [XmlElement("id")]
         public int Id { get; set; }
 
+        [XmlElement("last-modified")]
         public DateTime LastModified { get; set; }
 
+        [XmlElement("archive")]
         public virtual ViewArchive Archive { get; set; }
 
+        [XmlElement("application-user")]
         public virtual ViewApplicationUser ApplicationUser { get; set; }
     }
 }
--- Bondage.Portal/Bondage.Tier.ViewModels/Classes/Views/ViewArchive.cs
+++ Bondage.Portal/Bondage.Tier.ViewModels/Classes/Views/ViewArchive.cs
@@ -1,23 +1,29 @@
 ﻿using System;
-
+using System.Xml.Serialization;
 using Bondage.Tier.ViewModels.Interfaces.Views;
 
 namespace Bondage.Tier.ViewModels.Classes.Views
 {
+    [XmlRoot("archive")]
     public class ViewArchive : IViewKey, IViewBase
     {
         public ViewArchive()
         {
         }
 
+        [XmlElement("id")]
         public int Id { get; set; }
 
+        [XmlElement("last-modified")]
         public DateTime LastModified { get; set; }
 
+        [XmlElement("by")]
         public virtual ViewApplicationUser By { get; set; }
 
+        [XmlIgnore]
         public byte[] Data { get; set; }
 
+        [XmlElement("name")]
         public string Name { get; set; }
     }
 }
