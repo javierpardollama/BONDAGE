1ffbcb541f33f27e84bd3991810516c653a9a686
Javier Pardo <javierpardollama@outlook.com>
2020-04-20 22:11:29 +0200

    BORRADO SUAVE

--- Bondage.Portal/Bondage.Tier.Contexts/Extensions/FiltersExtension.cs
+++ Bondage.Portal/Bondage.Tier.Contexts/Extensions/FiltersExtension.cs
@@ -17,6 +17,7 @@ namespace Bondage.Tier.Contexts.Extensions
             @this.Entity<ApplicationUserRole>().HasQueryFilter(p => !p.Deleted);
             @this.Entity<ApplicationUserToken>().HasQueryFilter(p => !p.Deleted);
             @this.Entity<Endeavour>().HasQueryFilter(p => !p.Deleted);
+            @this.Entity<Kind>().HasQueryFilter(p => !p.Deleted);
         }
     }
 }
