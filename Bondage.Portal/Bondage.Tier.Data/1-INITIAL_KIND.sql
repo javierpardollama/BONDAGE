8a34c33e049520ccf6db691241b8687242e3318e
Javier Pardo <javierpardollama@outlook.com>
2021-02-23 11:26:30 +0100

    FICHEROS DE INICIALIZACION DE DATOS

--- Bondage.Portal/Bondage.Tier.Data/1-INITIAL_KIND.sql
+++ Bondage.Portal/Bondage.Tier.Data/1-INITIAL_KIND.sql
@@ -1,6 +1,6 @@
 DELETE FROM Kind;
 delete from sqlite_sequence where name='Kind';
-INSERT INTO Kind (LASTMODIFIED, NAME, DELETED, IMAGEURI) VALUES(date('now'),"Start", 0,"iconos/Start_500px.png" );
-INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Pause", 0, "iconos/Pause_500px.png");
-INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Resume", 0,"iconos/Resume_500px.png");
-INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Stop", 0,"iconos/Stop_500px.png");
+INSERT INTO Kind (LASTMODIFIED, NAME, DELETED, IMAGEURI) VALUES(date('now'),"Start", 0,"kind/Start_500px.png" );
+INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Pause", 0, "kind/Pause_500px.png");
+INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Resume", 0,"kind/Resume_500px.png");
+INSERT INTO Kind (LASTMODIFIED,NAME, DELETED, IMAGEURI) VALUES(date('now'),"Stop", 0,"kind/Stop_500px.png");
